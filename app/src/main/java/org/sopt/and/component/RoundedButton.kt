8e25f36bf526d6da0c47fc5f495e504705a50e35
김성민 <arcn1345@gmail.com>
2024-10-11 15:43:29 +0900

    [feat] #1 RoundedButton 추가

---
+++ app/src/main/java/org/sopt/and/component/RoundedButton.kt
@@ -0,0 +1,41 @@
+package org.sopt.and.component
+
+import android.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.ui.theme.WavveBlue
+import org.sopt.and.ui.theme.White
+
+@Composable
+fun RoundedButton(
+    content: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick  = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+            .background(
+                color = WavveBlue,
+                shape = RoundedCornerShape(30.dp)
+                )
+
+    ) {
+        Text(
+            text = content,
+            style = TextStyle(fontSize = 20.sp),
+            color = White
+        )
+    }
+}
