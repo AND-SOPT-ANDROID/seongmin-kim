5f57721537359539dbd50f345964470739f49955
김성민 <arcn1345@gmail.com>
2024-10-11 17:16:07 +0900

    [feat] #1 SignTextField 비밀번호 show/hied 추가

--- app/src/main/java/org/sopt/and/component/SignTextField.kt
+++ app/src/main/java/org/sopt/and/component/SignTextField.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.sopt.and.R
 import org.sopt.and.ui.theme.DoubleDarkGray
 import org.sopt.and.ui.theme.Gray
 import org.sopt.and.ui.theme.LittleGray
@@ -23,8 +29,9 @@ fun SignTextField(
     value: String,
     onValueChange: (String) -> Unit,
     placeholder: String,
-    modifier: Modifier,
-    isPwdVisible: Boolean = false
+    modifier: Modifier = Modifier,
+    isPwdVisible: Boolean = false,
+    onPwdVisibilityChange: (() -> Unit)? = null
 ) {
     BasicTextField(
         value = value,
@@ -41,10 +48,11 @@ fun SignTextField(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 16.dp, horizontal = 16.dp)
+                    .padding(vertical = 16.dp, horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Box(
-
+                    modifier = Modifier.weight(1f)
                 ) {
                     if(value.isEmpty()) {
                         Text(
@@ -54,8 +62,22 @@ fun SignTextField(
                     }
                     innerTextField()
                 }
+
+                // 비밀번호 show/hide
+                if (onPwdVisibilityChange != null) {
+                    TextButton(
+                        onClick = onPwdVisibilityChange,
+                        modifier = Modifier.padding(start = 8.dp),
+                    ) {
+                        Text(
+                            text = if (isPwdVisible) stringResource(R.string.show) else stringResource(R.string.hide),
+                            color = White
+                        )
+                    }
+                }
             }
-        }
+        },
+        visualTransformation = if (isPwdVisible) VisualTransformation.None else PasswordVisualTransformation()
     )
 
 }
