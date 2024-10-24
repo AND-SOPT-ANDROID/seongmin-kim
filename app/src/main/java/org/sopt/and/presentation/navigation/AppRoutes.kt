621f77284576d2e31edb0afa9e6c961b37003e1c
ësungmin <arcn1345@gmail.com>
2024-10-24 23:32:42 +0900

    [feat] : #3 Route 정의

---
+++ app/src/main/java/org/sopt/and/presentation/navigation/AppRoutes.kt
@@ -0,0 +1,8 @@
+package org.sopt.and.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+object AppRoutes {
+    const val SignInScreen = "sign_in"
+    const val SignUpScreen = "sign_up"
+}
