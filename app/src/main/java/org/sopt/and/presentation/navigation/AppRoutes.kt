ea93dd2eb9bdd605bed5ee5226931559c8418833
ësungmin <arcn1345@gmail.com>
2024-10-25 02:50:55 +0900

    [del] : #3 AppRoutes 삭제

--- app/src/main/java/org/sopt/and/presentation/navigation/AppRoutes.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.and.presentation.navigation
-
-import kotlinx.serialization.Serializable
-
-object AppRoutes {
-    const val SignInScreen = "sign_in"
-    const val SignUpScreen = "sign_up"
-}
