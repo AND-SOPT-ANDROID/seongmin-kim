a6ed8047ea2e90791505e2dfbd21705476b44ec0
ësungmin <arcn1345@gmail.com>
2024-10-25 05:21:59 +0900

    [feat] : #3 MyPage Email 전달 부분 제거

--- app/src/main/java/org/sopt/and/presentation/navigation/Navigation.kt
+++ app/src/main/java/org/sopt/and/presentation/navigation/Navigation.kt
@@ -8,11 +8,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
+import org.sopt.and.presentation.screen.home.HomeScreen
 import org.sopt.and.presentation.screen.mypage.MyPageScreen
+import org.sopt.and.presentation.screen.search.SearchScreen
 import org.sopt.and.presentation.screen.signin.SignInScreen
 import org.sopt.and.presentation.screen.signup.SignUpScreen
 import org.sopt.and.presentation.viewmodel.SignInViewModel
@@ -20,23 +21,24 @@ import org.sopt.and.presentation.viewmodel.SignUpViewModel
 
 @Composable
 fun Navigation(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavHostController
 ) {
    Scaffold (
        modifier = Modifier.fillMaxSize()
    ) { innerPadding ->
-       val navController = rememberNavController()
+
        NavHost(
            navController = navController,
-           startDestination = Routes.SignInScreen
+           startDestination = Routes.SignInScreen.route
        ) {
-           composable<Routes.SignInScreen> {
+           composable(Routes.SignInScreen.route) {
                val signInViewModel: SignInViewModel = viewModel()
                val signInEmail = signInViewModel.signInState.collectAsState().value.email
                SignInScreen(
                    navigateUp = { navController.popBackStack() },
-                   navigateSignUp = { navController.navigate(Routes.SignUpScreen) },
-                   navigateMyPage = {navController.navigate(Routes.MyPageScreen(signInEmail))},
+                   navigateSignUp = { navController.navigate(Routes.SignUpScreen.route) },
+                   navigateMyPage = {navController.navigate(Routes.MyPageScreen.route)},
                    signInEmail = signInEmail,
                    signInPwd = signInViewModel.signInState.collectAsState().value.password,
                    onEmailChange = { signInViewModel.setSignInEmail(it) },
@@ -49,11 +51,11 @@ fun Navigation(
                )
            }
 
-           composable<Routes.SignUpScreen> {
+           composable(Routes.SignUpScreen.route) {
                val signUpViewModel: SignUpViewModel = viewModel()
                SignUpScreen(
                    navigateUp = { navController.popBackStack() },
-                   navigateSignIn = { navController.navigate(Routes.SignInScreen) },
+                   navigateSignIn = { navController.navigate(Routes.SignInScreen.route) },
                    signUpEmail = signUpViewModel.signUpState.collectAsState().value.email,
                    signUpPwd = signUpViewModel.signUpState.collectAsState().value.password,
                    onEmailChange = { signUpViewModel.setSignUpEmail(it) },
@@ -65,11 +67,18 @@ fun Navigation(
                )
            }
 
-           composable<Routes.MyPageScreen> {
-               val args = it.toRoute<Routes.MyPageScreen>()
-               MyPageScreen(
-                   email = args.email
-               )
+           composable(Routes.MyPageScreen.route) { //backStackEntry ->
+               //val email = backStackEntry.arguments?.getString("email") ?: ""
+               MyPageScreen()
+           }
+
+
+           composable(Routes.SearchScreen.route) {
+               SearchScreen()
+           }
+
+           composable(Routes.HomeScreen.route) {
+               HomeScreen()
            }
        }
    }
--- app/src/main/java/org/sopt/and/presentation/screen/mypage/MyPageScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/screen/mypage/MyPageScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,7 @@ import org.sopt.and.ui.theme.White
 
 @Composable
 fun MyPageScreen (
-    email: String
+
 ) {
     Column(
         modifier = Modifier
@@ -66,7 +67,7 @@ fun MyPageScreen (
                         .size(48.dp)
                 )
                 Text(
-                    text = email,
+                    text = stringResource(R.string.signup_id_placeholder),
                     style = TextStyle(
                         color = White,
                         fontSize = 18.sp
